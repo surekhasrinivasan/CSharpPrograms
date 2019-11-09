1ac581d32b055d3f0913de487336746764e62f42
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-08 20:06:21 -0500

    CSharp programs for Fundamental Mathematical Operations

---
+++ CSharpExamplesForMathematicalOperations/CSharpExamplesForMathematicalOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExamplesForMathematicalOperations
+{
+    // Program to Check whether the Entered Number is Even or Odd
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
