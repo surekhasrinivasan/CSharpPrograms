6399e01a08d396622dff88fd11f22a1e8b309bf2
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-10 19:07:07 -0500

    Program to Get a number and display the sum of the digits

--- CSharpExamplesForMathematicalOperations/CSharpExamplesForMathematicalOperations/Program.cs
+++ CSharpExamplesForMathematicalOperations/CSharpExamplesForMathematicalOperations/Program.cs
@@ -29,23 +29,29 @@ namespace CSharpExamplesForMathematicalOperations
     //    }
     //}
 
-    // Program to Swap 2 numbers 
+    //// Program to Swap 2 numbers 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int num1, num2, temp;
+    //        Console.Write("Enter the First Number: ");
+    //        num1 = int.Parse(Console.ReadLine());
+    //        Console.Write("Enter the Second Number: ");
+    //        num2 = int.Parse(Console.ReadLine());
+    //        temp = num1;
+    //        num1 = num2;
+    //        num2 = temp;
+    //        Console.Write("After Swapping : ");
+    //        Console.Write("\nFirst Number: " + num1);
+    //        Console.Write("\nSecond Number: " + num2);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to Get a number and display the sum of the digits 
     class Program
     {
-        static void Main(string[] args)
-        {
-            int num1, num2, temp;
-            Console.Write("Enter the First Number: ");
-            num1 = int.Parse(Console.ReadLine());
-            Console.Write("Enter the Second Number: ");
-            num2 = int.Parse(Console.ReadLine());
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
-            Console.Write("After Swapping : ");
-            Console.Write("\nFirst Number: " + num1);
-            Console.Write("\nSecond Number: " + num2);
-            Console.Read();
-        }
+
     }
 }
