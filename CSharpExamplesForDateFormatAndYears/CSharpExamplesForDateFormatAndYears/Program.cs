47cb51e529dec0988294f180258596da848df82a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-14 17:57:45 -0500

    Program to check whether the entered year is a leap year or not

---
+++ CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExamplesForDateFormatAndYears
+{
+    // Program to check whether the entered year is a leap year or not
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
