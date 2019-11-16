6398a9209f5c0f5a537db514a7d24bedc5654626
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-15 20:58:03 -0500

    Program Displays the Date in various Formats

--- CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
+++ CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
@@ -44,6 +44,14 @@ namespace CSharpExamplesForDateFormatAndYears
             DateTime date = new DateTime(2019, 11, 15);
             Console.WriteLine("Some Date Formats : ");
             Console.WriteLine("Date and Time: {0}", date);
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("dd-MMM-yy"));
+            Console.WriteLine(date.ToString("M/d/yyyy"));
+            Console.WriteLine(date.ToString("M/d/yy"));
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+            Console.WriteLine(date.ToString("MM/dd/yy"));
+            Console.WriteLine(date.ToString("yy/MM/dd"));
+            Console.Read();
         }
     }
 }
