d795424a272ea74a9fb598f381faec5d51dd3586
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-15 20:07:44 -0500

    Program to Display the Date in various Formats

--- CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
+++ CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
@@ -6,33 +6,39 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesForDateFormatAndYears
 {
-    // Program to check whether the entered year is a leap year or not
+    //// Program to check whether the entered year is a leap year or not
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Program obj = new Program();
+    //        obj.readdata();
+    //        obj.leap();
+    //    }
+
+    //    int year;
+    //    public void readdata()
+    //    {
+    //        Console.WriteLine("Enter the Year in Four Digits: ");
+    //        year = Convert.ToInt32(Console.ReadLine());
+    //    }
+
+    //    public void leap()
+    //    {
+    //        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    //        {
+    //            Console.WriteLine("{0} is a Leap Year", year);
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("{0} is not a Leap Year", year);
+    //        }
+    //    }
+    //}
+
+    // Program to Display the Date in various Formats
     class Program
     {
-        static void Main(string[] args)
-        {
-            Program obj = new Program();
-            obj.readdata();
-            obj.leap();
-        }
 
-        int year;
-        public void readdata()
-        {
-            Console.WriteLine("Enter the Year in Four Digits: ");
-            year = Convert.ToInt32(Console.ReadLine());
-        }
-
-        public void leap()
-        {
-            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-            {
-                Console.WriteLine("{0} is a Leap Year", year);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a Leap Year", year);
-            }
-        }
     }
 }
