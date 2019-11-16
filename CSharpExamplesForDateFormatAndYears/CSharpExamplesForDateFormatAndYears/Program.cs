5fdb5097856de9721b682b36bbecb8edfb44def0
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-16 16:50:27 -0500

    Program to compare two Dates

--- CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
+++ CSharpExamplesForDateFormatAndYears/CSharpExamplesForDateFormatAndYears/Program.cs
@@ -36,22 +36,28 @@ namespace CSharpExamplesForDateFormatAndYears
     //    }
     //}
 
-    // Program to Display the Date in various Formats
+    //// Program to Display the Date in various Formats
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        DateTime date = new DateTime(2019, 11, 15);
+    //        Console.WriteLine("Some Date Formats : ");
+    //        Console.WriteLine("Date and Time: {0}", date);
+    //        Console.WriteLine(date.ToString("yyyy-MM-dd"));
+    //        Console.WriteLine(date.ToString("dd-MMM-yy"));
+    //        Console.WriteLine(date.ToString("M/d/yyyy"));
+    //        Console.WriteLine(date.ToString("M/d/yy"));
+    //        Console.WriteLine(date.ToString("MM/dd/yyyy"));
+    //        Console.WriteLine(date.ToString("MM/dd/yy"));
+    //        Console.WriteLine(date.ToString("yy/MM/dd"));
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to compare two Dates
     class Program
     {
-        static void Main(string[] args)
-        {
-            DateTime date = new DateTime(2019, 11, 15);
-            Console.WriteLine("Some Date Formats : ");
-            Console.WriteLine("Date and Time: {0}", date);
-            Console.WriteLine(date.ToString("yyyy-MM-dd"));
-            Console.WriteLine(date.ToString("dd-MMM-yy"));
-            Console.WriteLine(date.ToString("M/d/yyyy"));
-            Console.WriteLine(date.ToString("M/d/yy"));
-            Console.WriteLine(date.ToString("MM/dd/yyyy"));
-            Console.WriteLine(date.ToString("MM/dd/yy"));
-            Console.WriteLine(date.ToString("yy/MM/dd"));
-            Console.Read();
-        }
+
     }
 }
