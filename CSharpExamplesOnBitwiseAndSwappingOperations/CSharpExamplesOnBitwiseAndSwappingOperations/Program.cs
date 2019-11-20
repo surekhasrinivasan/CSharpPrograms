69eb7d1680c6db43a6c5c7d7a26e8ed662e642f5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-19 19:44:28 -0500

    Program to Find the Frequency of the Word ʺtheʺ in a given Sentence

--- CSharpExamplesOnBitwiseAndSwappingOperations/CSharpExamplesOnBitwiseAndSwappingOperations/Program.cs
+++ CSharpExamplesOnBitwiseAndSwappingOperations/CSharpExamplesOnBitwiseAndSwappingOperations/Program.cs
@@ -6,30 +6,36 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnBitwiseAndSwappingOperations
 {
-    // Program to Accept the Height of a Person & Categorize as Taller, Dwarf and Average
+    //// Program to Accept the Height of a Person & Categorize as Taller, Dwarf and Average
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        float height;
+    //        Console.WriteLine("Enter the Height (in centimeters) \n");
+    //        height = int.Parse(Console.ReadLine());
+    //        if(height < 150.0)
+    //        {
+    //            Console.WriteLine("Dwarf \n");
+    //        }
+    //        else if((height >= 150.0) && (height <= 165.0))
+    //        {
+    //            Console.WriteLine("Average Height \n");
+    //        }
+    //        else if ((height >= 165.0) && (height <= 195.0))
+    //        {
+    //            Console.WriteLine("Taller \n");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Abnormal height \n");
+    //        }
+    //    }
+    //}
+
+    // Program to Find the Frequency of the Word ʺtheʺ in a given Sentence
     class Program
     {
-        static void Main(string[] args)
-        {
-            float height;
-            Console.WriteLine("Enter the Height (in centimeters) \n");
-            height = int.Parse(Console.ReadLine());
-            if(height < 150.0)
-            {
-                Console.WriteLine("Dwarf \n");
-            }
-            else if((height >= 150.0) && (height <= 165.0))
-            {
-                Console.WriteLine("Average Height \n");
-            }
-            else if ((height >= 165.0) && (height <= 195.0))
-            {
-                Console.WriteLine("Taller \n");
-            }
-            else
-            {
-                Console.WriteLine("Abnormal height \n");
-            }
-        }
+
     }
 }
