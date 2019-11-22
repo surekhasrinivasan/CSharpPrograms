36fe9939e107b7fc0210fa02ad701696866cdcd5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-22 16:43:12 -0500

    Program to Print a Diamond Using Nested Loop

--- CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
+++ CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
@@ -6,42 +6,48 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnAccessSpecifiers
 {
-    // Program to Illustrate the Use of Access Specifiers
+    //// Program to Illustrate the Use of Access Specifiers
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Two B = new Two();
+    //        B.show();
+    //    }
+    //}
+
+    //class One
+    //{
+    //    private int x;
+    //    protected int y;
+    //    internal int z;
+    //    public int a;
+    //    protected internal int b;
+    //}
+
+    //class Two : One
+    //{
+    //    public void show()
+    //    {
+    //        Console.WriteLine("Values are : ");
+    //        //x = 10;
+    //        y = 20;
+    //        z = 30;
+    //        a = 40;
+    //        b = 50;
+
+    //        //Console.WriteLine(x); // Error - x is not accessible 
+    //        Console.WriteLine(y);
+    //        Console.WriteLine(z);
+    //        Console.WriteLine(a);
+    //        Console.WriteLine(b);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to Print a Diamond Using Nested Loop
     class Program
     {
-        static void Main(string[] args)
-        {
-            Two B = new Two();
-            B.show();
-        }
-    }
 
-    class One
-    {
-        private int x;
-        protected int y;
-        internal int z;
-        public int a;
-        protected internal int b;
-    }
-
-    class Two : One
-    {
-        public void show()
-        {
-            Console.WriteLine("Values are : ");
-            //x = 10;
-            y = 20;
-            z = 30;
-            a = 40;
-            b = 50;
-
-            //Console.WriteLine(x); // Error - x is not accessible 
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.ReadLine();
-        }
     }
 }
