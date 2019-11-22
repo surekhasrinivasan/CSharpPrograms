a7cb8418961458007774d9e5093089e1338d9393
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-22 16:52:50 -0500

    Program to Print a Diamond Using Nested Loop-completed

--- CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
+++ CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
@@ -65,6 +65,17 @@ namespace CSharpExamplesOnAccessSpecifiers
                     Console.Write("*");
                 Console.WriteLine();
             }
+            count = 1;
+            for (k = 1; k <= number - 1; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - k) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
