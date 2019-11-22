5c581c808d4adc4f4135970f645fe2d73dac8fec
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-22 16:48:53 -0500

    Prints half of the diamond shape

--- CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
+++ CSharpExamplesOnAccessSpecifiers/CSharpExamplesOnAccessSpecifiers/Program.cs
@@ -54,6 +54,17 @@ namespace CSharpExamplesOnAccessSpecifiers
 
             Console.Write("Enter number of rows \n");
             number = int.Parse(Console.ReadLine());
+
+            count = number - 1;
+            for(k = 1; k <= number; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * k - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
         }
     }
 }
