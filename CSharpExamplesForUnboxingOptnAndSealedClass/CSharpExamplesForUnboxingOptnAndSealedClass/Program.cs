4b4d8d929b060ad323f83e9555ece99fc8552bc4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-29 08:02:28 -0500

    Program to create sealed class

--- CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
+++ CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesForUnboxingOptnAndSealedClass
 {
-    // Program to display Squarefeet of a House 
+    //// Program to display Squarefeet of a House 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int length, width, area;
+    //        Console.WriteLine("Enter the length of room in feet: ");
+    //        length = Convert.ToInt32(Console.ReadLine());
+    //        Console.WriteLine("Enter the width of room in feet: ");
+    //        width = Convert.ToInt32(Console.ReadLine());
+
+    //        area = length * width;
+    //        Console.WriteLine("House is " + area + " square feet.");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to create Sealed Class
     class Program
     {
-        static void Main(string[] args)
-        {
-            int length, width, area;
-            Console.WriteLine("Enter the length of room in feet: ");
-            length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the width of room in feet: ");
-            width = Convert.ToInt32(Console.ReadLine());
 
-            area = length * width;
-            Console.WriteLine("House is " + area + " square feet.");
-            Console.ReadLine();
-        }
     }
 }
