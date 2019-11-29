afe350e90d9c506757e28bafc82d0b64205442ca
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-29 08:06:53 -0500

    Program to create a Sealed class - completed

--- CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
+++ CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
@@ -29,6 +29,10 @@ namespace CSharpExamplesForUnboxingOptnAndSealedClass
         static void Main()
         {
             SealedClass sc = new SealedClass();
+            sc.x = 100;
+            sc.y = 180;
+            Console.WriteLine("x = {0}, y = {1}", sc.x, sc.y);
+            Console.ReadLine();
         }
     }
 
