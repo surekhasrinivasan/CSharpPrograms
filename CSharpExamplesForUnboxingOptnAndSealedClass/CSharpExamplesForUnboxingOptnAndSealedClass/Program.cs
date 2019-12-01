190417cbca62f3e3e6dcaab5010a95075746fd3f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-01 00:07:13 -0500

    logic to find the prime numbers between 1 and 100

--- CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
+++ CSharpExamplesForUnboxingOptnAndSealedClass/CSharpExamplesForUnboxingOptnAndSealedClass/Program.cs
@@ -73,6 +73,22 @@ namespace CSharpExamplesForUnboxingOptnAndSealedClass
         {
             bool isPrime = true;
             Console.WriteLine("Prime Numbers: ");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("\t" + i);
+                }
+                isPrime = true;
+            }
         }
     }
 }
