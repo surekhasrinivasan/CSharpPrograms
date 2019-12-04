c85e99626ac0990d6cebacdd4a5ef9e47651620c
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-04 17:49:13 -0500

    Program to compute average for the set of values

--- CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
+++ CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
@@ -6,26 +6,32 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnNumericalOperations
 {
-    // Program to find Magnitude of integer
+    //// Program to find Magnitude of integer
+    //class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        int num, magnitude = 0;
+    //        Console.WriteLine("Enter the number: ");
+    //        num = int.Parse(Console.ReadLine());
+
+    //        Console.WriteLine("Number: " +num);
+    //        while (num > 0)
+    //        {
+    //            magnitude++;
+    //            num = num / 10;
+    //        };
+
+    //        // Magnitude of an integer is the length of the integer 
+    //        // which is obtained using the mod function 
+    //        Console.WriteLine("Magnitude: " + magnitude);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to compute average for the set of values
     class Program
     {
-        public static void Main()
-        {
-            int num, magnitude = 0;
-            Console.WriteLine("Enter the number: ");
-            num = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Number: " +num);
-            while (num > 0)
-            {
-                magnitude++;
-                num = num / 10;
-            };
-
-            // Magnitude of an integer is the length of the integer 
-            // which is obtained using the mod function 
-            Console.WriteLine("Magnitude: " + magnitude);
-            Console.Read();
-        }
     }
 }
