4dc5af137b2dae212501f96cf1ea44d7b2e2feaa
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-05 17:24:20 -0500

    Program to display Numbers in the form of Triangle-completed

--- CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
+++ CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
@@ -67,7 +67,22 @@ namespace CSharpExamplesOnNumericalOperations
                     // for loop to print spaces 
                     Console.Write(" ");
                 }
+
+                for(int j = 0; j < i; j++)
+                {
+                    if(j == 0 || i == j)
+                    {
+                        arr[i, j] = 1;
+                    }
+                    else
+                    {
+                        arr[i, j] = arr[i - 1, j] + arr[i - 1, j - 1];
+                    }
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
             }
+            Console.ReadLine();
         }
     }
 }
