9f3d62fc377ec0a1204b0be13b182edc5832c762
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-05 17:07:00 -0500

    Program to display Numbers in the form of Triangle

--- CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
+++ CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
@@ -29,28 +29,34 @@ namespace CSharpExamplesOnNumericalOperations
     //    }
     //}
 
-    // Program to compute average for the set of values
+    //// Program to compute average for the set of values
+    //class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        int m, i, sum = 0, average = 0;
+    //        Console.WriteLine("Enter the Number of Terms in the Array ");
+    //        m = int.Parse(Console.ReadLine());
+
+    //        int[] a = new int[m];
+    //        Console.WriteLine("Enter the Array Elements ");
+    //        for(i = 0; i < m; i++)
+    //        {
+    //            a[i] = int.Parse(Console.ReadLine());
+    //        }
+    //        for(i = 0; i < m; i++)
+    //        {
+    //            sum += a[i];
+    //        }
+    //        average = sum / m;
+    //        Console.WriteLine("Average is {0}", average);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to display Numbers in the form of Triangle
     class Program
     {
-        public static void Main()
-        {
-            int m, i, sum = 0, average = 0;
-            Console.WriteLine("Enter the Number of Terms in the Array ");
-            m = int.Parse(Console.ReadLine());
 
-            int[] a = new int[m];
-            Console.WriteLine("Enter the Array Elements ");
-            for(i = 0; i < m; i++)
-            {
-                a[i] = int.Parse(Console.ReadLine());
-            }
-            for(i = 0; i < m; i++)
-            {
-                sum += a[i];
-            }
-            average = sum / m;
-            Console.WriteLine("Average is {0}", average);
-            Console.ReadLine();
-        }
     }
 }
