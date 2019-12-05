b196e87a0c5f50f27bc794fb627c0148781aff28
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-05 17:19:46 -0500

    create a loop to print spaces

--- CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
+++ CSharpExamplesOnNumericalOperations/CSharpExamplesOnNumericalOperations/Program.cs
@@ -60,6 +60,14 @@ namespace CSharpExamplesOnNumericalOperations
         public static void Main()
         {
             int[,] arr = new int[8, 8];
+            for(int i = 0; i < 8; i++)
+            {
+                for(int k = 7; k > i; k--)
+                {
+                    // for loop to print spaces 
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
