87925688844fd0441d2d7e3751b0d42ae437ae12
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-06 22:51:13 -0500

    Use StreamReader to read the file

--- CSharpExamplesOnInterfaceAndLoopingOperation/CSharpExamplesOnInterfaceAndLoopingOperation/Program.cs
+++ CSharpExamplesOnInterfaceAndLoopingOperation/CSharpExamplesOnInterfaceAndLoopingOperation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,30 @@ namespace CSharpExamplesOnInterfaceAndLoopingOperation
     // Program to implement PhoneBook
     class Program
     {
-       static void Main()
+       static void Main(string[] arg)
         {
             Hashtable tab = new Hashtable();
             string fileName;
+            if(arg.Length > 0)
+            {
+                fileName = arg[0];
+            }
+            else
+            {
+                fileName = "phoneBook.txt";
+            }
+            StreamReader r = File.OpenText(fileName);
+            string line = r.ReadLine();
+            while(line != null)
+            {
+                int pos = line.IndexOf('=');
+                string name = line.Substring(0, pos).Trim();
+                long phone = Convert.ToInt64(line.Substring(pos + 1));
+                tab[name] = phone;
+                line = r.ReadLine();
+            }
+            r.Close();
+                
         }
     }
 }
