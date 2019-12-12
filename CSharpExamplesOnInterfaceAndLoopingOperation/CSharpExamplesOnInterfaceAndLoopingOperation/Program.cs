48f60ca3b3d6c47ee9ce6dea27c6f59995149ecb
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-11 23:07:06 -0500

    Program to perform division of Exponents of same base-completed

--- CSharpExamplesOnInterfaceAndLoopingOperation/CSharpExamplesOnInterfaceAndLoopingOperation/Program.cs
+++ CSharpExamplesOnInterfaceAndLoopingOperation/CSharpExamplesOnInterfaceAndLoopingOperation/Program.cs
@@ -174,7 +174,11 @@ namespace CSharpExamplesOnInterfaceAndLoopingOperation
             Console.WriteLine("Enter the Second Exponent: ");
             double exp2 = double.Parse(Console.ReadLine());
 
+            double div;
 
+            div = exp1 - exp2;
+            Console.WriteLine("Result is : {0}^{1} : {2}", num, div, Math.Pow(num, div));
+            Console.ReadLine();
         }
     }
 }
