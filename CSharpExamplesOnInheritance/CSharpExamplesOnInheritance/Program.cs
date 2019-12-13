b30fccef703772c22669f9ac2d1035dbf8cf31cd
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-13 17:20:46 -0500

    Program to illustrate single inheritance

--- CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
+++ CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
@@ -6,38 +6,45 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnInheritance
 {
-    // Program to demonstrate Multilevel Inheritance 
-    class Program : Vehicle
-    {
-        public void Noise()
-        {
-            Console.WriteLine("All vehicles creates noise !!");
-        }
+    //// Program to demonstrate Multilevel Inheritance 
+    //class Program : Vehicle
+    //{
+    //    public void Noise()
+    //    {
+    //        Console.WriteLine("All vehicles creates noise !!");
+    //    }
 
-        static void Main(string[] args)
-        {
-            Program obj = new Program();
-            obj.mode();
-            obj.feature();
-            obj.Noise();
-            Console.Read();
-        }
+    //    static void Main(string[] args)
+    //    {
+    //        Program obj = new Program();
+    //        obj.mode();
+    //        obj.feature();
+    //        obj.Noise();
+    //        Console.Read();
+    //    }
+    //}
+
+    //class Mode
+    //{
+    //    public void mode()
+    //    {
+    //        Console.WriteLine("There are many modes of Transport!!");
+    //    }
+    //}
+
+    //class Vehicle : Mode
+    //{
+    //    public void feature()
+    //    {
+    //        Console.WriteLine("They mainly help in travelling !!");
+    //    }
+    //}
+
+    // Program to illustrate Single inheritance
+    class Program
+    {
+
     }
 
-    class Mode
-    {
-        public void mode()
-        {
-            Console.WriteLine("There are many modes of Transport!!");
-        }
-    }
-
-    class Vehicle : Mode
-    {
-        public void feature()
-        {
-            Console.WriteLine("They mainly help in travelling !!");
-        }
-    }
     
 }
