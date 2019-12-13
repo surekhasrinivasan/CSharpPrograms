7dd92ffc62e1b2dc4a4a3d6f66508f80dc36a35e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-12 20:06:54 -0500

    Program to demonstrate Multilevel Inheritance-completed

--- CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
+++ CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
@@ -7,26 +7,37 @@ using System.Threading.Tasks;
 namespace CSharpExamplesOnInheritance
 {
     // Program to demonstrate Multilevel Inheritance 
-    class Program
+    class Program : Vehicle
     {
+        public void Noise()
+        {
+            Console.WriteLine("All vehicles creates noise !!");
+        }
+
         static void Main(string[] args)
         {
-        }
-
-        class Mode
-        {
-            public void mode()
-            {
-                Console.WriteLine("There are many modes of Transport!!");
-            }
-        }
-
-        class Vehicle : Mode
-        {
-            public void feature()
-            {
-                Console.WriteLine("They mainly help in travelling !!");
-            }
+            Program obj = new Program();
+            obj.mode();
+            obj.feature();
+            obj.Noise();
+            Console.Read();
         }
     }
+
+    class Mode
+    {
+        public void mode()
+        {
+            Console.WriteLine("There are many modes of Transport!!");
+        }
+    }
+
+    class Vehicle : Mode
+    {
+        public void feature()
+        {
+            Console.WriteLine("They mainly help in travelling !!");
+        }
+    }
+    
 }
