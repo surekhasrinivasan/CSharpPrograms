bf4de07707043fe4b6bc2e90fe0af57410ba0a7d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-16 21:48:26 -0500

    create a Rectangle class, declare its properties and constructor

--- CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
+++ CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
@@ -161,4 +161,16 @@ namespace CSharpExamplesOnInheritance
     {
 
     }
+
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+    }
 }
