3c0de299580ae469d3a35287b842310776be21ba
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-17 16:30:49 -0500

    create a display method which includes base class display and cost display

--- CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
+++ CSharpExamplesOnInheritance/CSharpExamplesOnInheritance/Program.cs
@@ -195,7 +195,11 @@ namespace CSharpExamplesOnInheritance
             cost = GetArea() * 70;
             return cost;
         }
-
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine("Cost: {0}", costcal());
+        }
         
     }
 }
