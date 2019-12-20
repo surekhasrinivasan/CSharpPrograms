c12d695844a259f6d9402eae80b76ff1ed3b040f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-19 19:22:10 -0500

    Program to calculate size of File using Linq

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -7,26 +7,32 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnLinq
 {
-    // Program to count File Extensions and group it using Linq
+    //// Program to count File Extensions and group it using Linq
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string[] arr = {"aaa.txt", "bbb.TXT", "xyz.abc.pdf", "aaaa.PDF",
+    //                         "abc.xml", "ccc.txt", "zzz.txt"};
+
+    //        var egrp = arr.Select(file => Path.GetExtension(file).TrimStart('.').ToLower())
+    //                        .GroupBy(x => x, (ext, extCnt) => new
+    //                        {
+    //                            Extension = ext,
+    //                            Count = extCnt.Count()
+    //                        });
+
+    //        foreach(var v in egrp)
+    //        {
+    //            Console.WriteLine("{0} File(s) with {1} Extension", v.Count, v.Extension);
+    //        }
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to calculate size of File using Linq
     class Program
     {
-        static void Main(string[] args)
-        {
-            string[] arr = {"aaa.txt", "bbb.TXT", "xyz.abc.pdf", "aaaa.PDF",
-                             "abc.xml", "ccc.txt", "zzz.txt"};
 
-            var egrp = arr.Select(file => Path.GetExtension(file).TrimStart('.').ToLower())
-                            .GroupBy(x => x, (ext, extCnt) => new
-                            {
-                                Extension = ext,
-                                Count = extCnt.Count()
-                            });
-
-            foreach(var v in egrp)
-            {
-                Console.WriteLine("{0} File(s) with {1} Extension", v.Count, v.Extension);
-            }
-            Console.ReadLine();
-        }
     }
 }
