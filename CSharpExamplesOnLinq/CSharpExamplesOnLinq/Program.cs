fcd065167096c9d15c7eab150e114f261dabacaf
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-22 15:54:43 -0500

    Program to implement IEnumerable Interface using LINQ-completed

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -67,6 +67,11 @@ namespace CSharpExamplesOnLinq
             var t = typeof(IEnumerable);
             var typesIEnum = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
                                 x.GetTypes()).Where(x => t.IsAssignableFrom(x));
+            foreach(var types in typesIEnum)
+            {
+                Console.WriteLine(types.FullName);
+            }
+            Console.ReadLine();
         }
     }
 }
