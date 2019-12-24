5925bba60a786fc1d3d9b7684ba6c8680023ee6a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-23 22:46:28 -0500

    declare variable groups which is a collection of anonymous objects with a property min and max

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -81,6 +81,9 @@ namespace CSharpExamplesOnLinq
         static void Main(string[] args)
         {
             var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
+            var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
+                       group x.i by x.Grp into y
+                       select new { Min = y.Min(), Max = y.Max() };
         }
     }
 
