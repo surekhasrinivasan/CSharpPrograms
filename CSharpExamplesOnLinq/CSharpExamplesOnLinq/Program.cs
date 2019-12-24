2cc4b4b8812ece52254245d21f92e8bdf35b14c5
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-24 08:54:53 -0500

    Program to display the student details using Select Clause Linq

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -75,18 +75,24 @@ namespace CSharpExamplesOnLinq
     //    }
     //}
 
-    // Program to divide sequence into groups using Linq
+    //// Program to divide sequence into groups using Linq
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
+    //        var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
+    //                   group x.i by x.Grp into y
+    //                   select new { Min = y.Min(), Max = y.Max() };
+    //        foreach (var grp in grps)
+    //            Console.WriteLine("Min: " + grp.Min + " Max:" + grp.Max);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to display the student details using Select Clause Linq
     class Program
     {
-        static void Main(string[] args)
-        {
-            var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
-            var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
-                       group x.i by x.Grp into y
-                       select new { Min = y.Min(), Max = y.Max() };
-            foreach (var grp in grps)
-                Console.WriteLine("Min: " + grp.Min + " Max:" + grp.Max);
-            Console.ReadLine();
-        }
+
     }
 }
