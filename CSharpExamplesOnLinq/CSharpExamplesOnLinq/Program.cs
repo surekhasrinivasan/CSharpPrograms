56efcb5aa1c37d2c958a4764cf1033e23a5da764
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-27 09:09:35 -0500

    declare a variable which is an integer array

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -173,6 +173,9 @@ namespace CSharpExamplesOnLinq
     // Program to Display the Greatest numbers in an Array using WHERE Clause LINQ
     class Program
     {
-
+        static void Main()
+        {
+            int[] numbers = { 500, 344, 221, 4443, 229, 1008, 6000, 767, 256, 0 };
+        }
     }
 }
