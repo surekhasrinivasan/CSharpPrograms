87d18892b588ac9e52e778b2d049baf0c5b17418
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-28 09:33:30 -0500

    Program to Display the Smallest numbers in an Array using FROM Clause Linq

--- CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
+++ CSharpExamplesOnLinq/CSharpExamplesOnLinq/Program.cs
@@ -170,23 +170,29 @@ namespace CSharpExamplesOnLinq
     //    }
     //}
 
-    // Program to Display the Greatest numbers in an Array using WHERE Clause LINQ
+    //// Program to Display the Greatest numbers in an Array using WHERE Clause LINQ
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        int[] numbers = { 500, 344, 221, 4443, 229, 1008, 6000, 767, 256, 0 };
+
+    //        var greaterNums =
+    //        from num in numbers
+    //        where num > 500
+    //        select num;
+    //        Console.WriteLine("Numbers Greater than 500 :");
+    //        foreach (var s in greaterNums)
+    //        {
+    //            Console.Write(s.ToString() + " ");
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to Display the Smallest numbers in an Array using FROM Clause Linq
     class Program
     {
-        static void Main()
-        {
-            int[] numbers = { 500, 344, 221, 4443, 229, 1008, 6000, 767, 256, 0 };
 
-            var greaterNums =
-            from num in numbers
-            where num > 500
-            select num;
-            Console.WriteLine("Numbers Greater than 500 :");
-            foreach (var s in greaterNums)
-            {
-                Console.Write(s.ToString() + " ");
-            }
-            Console.Read();
-        }
     }
 }
