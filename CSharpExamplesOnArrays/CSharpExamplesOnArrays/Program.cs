c858588a3401ff8bda1b665a34250a4242956f13
Surekhasrinivasan <surekhasrao@gmail.com>
2019-12-31 07:09:30 -0500

    create convert method to convert the 2d input array to 1d

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -46,5 +46,17 @@ namespace CSharpExamplesOnArrays
             }
         }
 
+        public void convert()
+        {
+            int k = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    b[k++] = a[i, j];
+                }
+            }
+        }
+
     }
 }
