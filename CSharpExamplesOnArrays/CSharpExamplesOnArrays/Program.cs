605b19c9d9f64db32c668c988da459db368b34c1
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-03 17:12:28 -0500

    finding the long length of array using the LongLength method

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -94,6 +94,9 @@ namespace CSharpExamplesOnArrays
             int[] arrayA = new int[5];
             int lengthA = arrayA.Length;
             Console.WriteLine("Length of ArrayA : {0}", lengthA);
+
+            long longLength = arrayA.LongLength;
+            Console.WriteLine("Length of the LongLength Array : {0}", longLength);
         }
     }
 }
