6a32f9eb51baebc0a5e46c46ad38871297a87e77
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-03 17:01:10 -0500

    Program to get the length of the Array

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -6,84 +6,90 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnArrays
 {
-    // Program to Convert a 2D Array into 1D Array
+    //// Program to Convert a 2D Array into 1D Array
+    //class Program
+    //{
+    //    int m, n;
+    //    int[,] a;
+    //    int[] b;
+
+    //    Program(int x, int y)
+    //    {
+    //        m = x;
+    //        n = y;
+    //        a = new int[m, n];
+    //        b = new int[m * n];
+    //    }
+
+    //    public void readmatrix()
+    //    {
+    //        for (int i = 0; i < m; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                Console.WriteLine("a[{0},{1}]=", i, j);
+    //                a[i, j] = Convert.ToInt32(Console.ReadLine());
+    //            }
+    //        }
+    //    }
+
+    //    public void printd()
+    //    {
+    //        for (int i = 0; i < m; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                Console.Write("{0}\t", a[i, j]);
+
+    //            }
+    //            Console.Write("\n");
+    //        }
+    //    }
+
+    //    public void convert()
+    //    {
+    //        int k = 0;
+    //        for (int i = 0; i < m; i++)
+    //        {
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                b[k++] = a[i, j];
+    //            }
+    //        }
+    //    }
+
+    //    public void printoned()
+    //    {
+    //        for (int i = 0; i < m * n; i++)
+    //        {
+    //            Console.WriteLine("{0}\t", b[i]);
+    //        }
+    //    }
+
+    //    // In this program we read the elements of the 2-Dimensional matrix. 
+    //    // Using for loop assign the value of ‘a[i,j]’ variable to b[] 
+    //    // array variable. Increment the value of base index ‘k’ variable. 
+    //    // Print the value of one dimensional array.
+
+    //    public static void Main()
+    //    {
+    //        Program obj = new Program(2, 3);
+    //        Console.WriteLine("Enter the Elements : ");
+    //        obj.readmatrix();
+    //        Console.WriteLine("\t\t Given 2-D Array(Matrix) is : ");
+    //        obj.printd();
+    //        obj.convert();
+    //        Console.WriteLine("\t\t Converted 1-D Array is :");
+    //        obj.printoned();
+
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to get the length of the Array
     class Program
     {
-        int m, n;
-        int[,] a;
-        int[] b;
 
-        Program(int x, int y)
-        {
-            m = x;
-            n = y;
-            a = new int[m, n];
-            b = new int[m * n];
-        }
-
-        public void readmatrix()
-        {
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.WriteLine("a[{0},{1}]=", i, j);
-                    a[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-        }
-
-        public void printd()
-        {
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write("{0}\t", a[i, j]);
-
-                }
-                Console.Write("\n");
-            }
-        }
-
-        public void convert()
-        {
-            int k = 0;
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    b[k++] = a[i, j];
-                }
-            }
-        }
-
-        public void printoned()
-        {
-            for (int i = 0; i < m * n; i++)
-            {
-                Console.WriteLine("{0}\t", b[i]);
-            }
-        }
-
-        // In this program we read the elements of the 2-Dimensional matrix. 
-        // Using for loop assign the value of ‘a[i,j]’ variable to b[] 
-        // array variable. Increment the value of base index ‘k’ variable. 
-        // Print the value of one dimensional array.
-
-        public static void Main()
-        {
-            Program obj = new Program(2, 3);
-            Console.WriteLine("Enter the Elements : ");
-            obj.readmatrix();
-            Console.WriteLine("\t\t Given 2-D Array(Matrix) is : ");
-            obj.printd();
-            obj.convert();
-            Console.WriteLine("\t\t Converted 1-D Array is :");
-            obj.printoned();
-
-            Console.ReadLine();
-        }
     }
 }
 
