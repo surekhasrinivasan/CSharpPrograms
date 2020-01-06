a83aaa0e1f6e8cc05d6cca80977122c91981bbd1
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-05 19:28:15 -0500

    Program to Find the Rank of a given Array

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -104,25 +104,31 @@ namespace CSharpExamplesOnArrays
     //    }
     //}
 
-    // Program to Get Lower Bound and Upper Bound of an Array
+    //// Program to Get Lower Bound and Upper Bound of an Array
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Array stringArray = Array.CreateInstance(typeof(String), 6);
+    //        stringArray.SetValue("Mango", 0);
+    //        stringArray.SetValue("Orange", 1);
+    //        stringArray.SetValue("Apple", 2);
+    //        stringArray.SetValue("Grape", 3);
+    //        stringArray.SetValue("Cherry", 4);
+    //        stringArray.SetValue("Watermelon", 5);
+
+    //        Console.WriteLine("The Lower Bound of the Array : {0}", 
+    //                            stringArray.GetLowerBound(0).ToString());
+    //        Console.WriteLine("The Upper Bound of the Array : {0}",
+    //                        stringArray.GetUpperBound(0).ToString());
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to Find the Rank of a given Array
     class Program
     {
-        static void Main()
-        {
-            Array stringArray = Array.CreateInstance(typeof(String), 6);
-            stringArray.SetValue("Mango", 0);
-            stringArray.SetValue("Orange", 1);
-            stringArray.SetValue("Apple", 2);
-            stringArray.SetValue("Grape", 3);
-            stringArray.SetValue("Cherry", 4);
-            stringArray.SetValue("Watermelon", 5);
 
-            Console.WriteLine("The Lower Bound of the Array : {0}", 
-                                stringArray.GetLowerBound(0).ToString());
-            Console.WriteLine("The Upper Bound of the Array : {0}",
-                            stringArray.GetUpperBound(0).ToString());
-            Console.ReadLine();
-        }
     }
 }
 
