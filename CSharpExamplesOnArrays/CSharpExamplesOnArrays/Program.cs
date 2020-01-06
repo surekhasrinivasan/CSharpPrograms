0f6391f5c5aa3ae55c4e7d2ccce56f8342b67bf7
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-06 18:47:51 -0500

    Program to Demonstrate Jagged Arrays-completed

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -151,6 +151,15 @@ namespace CSharpExamplesOnArrays
             jag[0][1] = 12;
             jag[1] = new int[1] { 11 };
             jag[2] = new int[3] { 14, 15, 16 };
+            for(int i=0; i< jag.Length; i++)
+            {
+                int[] innerArray = jag[i];
+                for(int a = 0; a < innerArray.Length; a++)
+                {
+                    Console.WriteLine(innerArray[a] + " ");
+                }
+            }
+            Console.Read();
         }
     }
 }
