32c65934bfe9b5d5a7af028ed6188bc8040e2167
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-11 21:49:39 -0500

    create length property

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -249,6 +249,21 @@ namespace CSharpExamplesOnArrays
     class values
     {
         private int[] val = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        public int Length
+        {
+            get { return val.Length; }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return val[index];
+            }
+            set
+            {
+                val[index] = value;
+            }
+        }
     }
 }
 
