fb3b5605d857a05497f7f8565ee01373ac7787dd
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-15 21:15:13 -0500

    Program to Sort a List of Names in Alphabetical Order

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -290,36 +290,42 @@ namespace CSharpExamplesOnArrays
     //    }
     //}
 
-    // Program to Copy a Section of One Array to Another
+    //// Program to Copy a Section of One Array to Another
+    //class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        int n, m, size;
+    //        Console.WriteLine("Enter the size of the Array :");
+    //        n = Convert.ToInt32(Console.ReadLine());
+    //        int[] arr = new int[n];
+    //        Console.WriteLine("Enter the Elements of the First Array : ");
+    //        for(int i = 0; i < n; i++)
+    //        {
+    //            arr[i] = Convert.ToInt32(Console.ReadLine());
+    //        }
+
+    //        Console.WriteLine("Enter the Size of the Target Array : ");
+    //        m = Convert.ToInt32(Console.ReadLine());
+    //        int[] target = new int[m];
+    //        Console.WriteLine("Enter the section of the First Array " +
+    //                            "that has to be Copied : ");
+    //        size = Convert.ToInt32(Console.ReadLine());
+    //        Array.Copy(arr, 0, target, 0, size);
+    //        Console.WriteLine("New Array with the specified section of Elements " +
+    //                            " in the First Array");
+    //        foreach(int value in target)
+    //        {
+    //            Console.WriteLine(value);
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to Sort a List of Names in Alphabetical Order
     class Program
     {
-        public static void Main()
-        {
-            int n, m, size;
-            Console.WriteLine("Enter the size of the Array :");
-            n = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[n];
-            Console.WriteLine("Enter the Elements of the First Array : ");
-            for(int i = 0; i < n; i++)
-            {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
 
-            Console.WriteLine("Enter the Size of the Target Array : ");
-            m = Convert.ToInt32(Console.ReadLine());
-            int[] target = new int[m];
-            Console.WriteLine("Enter the section of the First Array " +
-                                "that has to be Copied : ");
-            size = Convert.ToInt32(Console.ReadLine());
-            Array.Copy(arr, 0, target, 0, size);
-            Console.WriteLine("New Array with the specified section of Elements " +
-                                " in the First Array");
-            foreach(int value in target)
-            {
-                Console.WriteLine(value);
-            }
-            Console.Read();
-        }
     }
 }
 
