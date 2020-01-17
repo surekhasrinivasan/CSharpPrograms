86bc573f0e832c2d917d755d47c103430368bfb2
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-16 21:27:25 -0500

    Program to Search an Element in an Array-completed

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -352,7 +352,9 @@ namespace CSharpExamplesOnArrays
                 element => element.StartsWith("cam", StringComparison.Ordinal));
             string v2 = Array.Find(arr,
                 element => element.Length == 3);
-
+            Console.WriteLine("The Element that starts with 'Cam' is : " + v1);
+            Console.WriteLine("3 Letter word in the Array is: " + v2);
+            Console.ReadLine();
         }
     }
 }
