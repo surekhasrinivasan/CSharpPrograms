5672c44cafd5c98effb7266e823e27e376874b93
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-17 16:02:09 -0500

    declare an integer array

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -361,7 +361,11 @@ namespace CSharpExamplesOnArrays
     // Program to Reverse an Array
     class Program
     {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+        }
     }
 }
 
