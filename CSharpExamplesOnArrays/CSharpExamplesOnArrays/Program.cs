2352ddca9bd6d0b35e8eb01b23af3e113e23db7a
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-17 16:03:17 -0500

    Print the elements in the array

--- CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
+++ CSharpExamplesOnArrays/CSharpExamplesOnArrays/Program.cs
@@ -364,6 +364,10 @@ namespace CSharpExamplesOnArrays
         static void Main()
         {
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach(int num in array)
+            {
+                Console.WriteLine(num);
+            }
 
         }
     }
