fa950d234a9c40f64b3913191be15352895a3b1b
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-22 18:15:45 -0500

    declare private members of the class

--- CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
+++ CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
@@ -9,6 +9,10 @@ namespace CSharpExamplesOnSorting
     // Program to perform Radix Sort
     class Program
     {
+        private int[] data;
+        private IList<IList<int>> digits = new List<IList<int>>();
+        private int maxLength = 0;
+
         static void Main(string[] args)
         {
         }
