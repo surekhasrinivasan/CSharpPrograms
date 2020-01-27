e66e03a72be4fc9cc99fe227b6bdd8a66efb6eaa
Surekhasrinivasan <surekhasrao@gmail.com>
2020-01-26 21:54:13 -0500

    Print the array elements before sorting

--- CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
+++ CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
@@ -90,6 +90,11 @@ namespace CSharpExamplesOnSorting
         {
             int array_size = 10;
             int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
+            Console.WriteLine("The Array before selection sort is : ");
+            for(int i = 0; i < array_size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 
