89c542893161f89e2ebd8fd38cc46a2fe632f9ed
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-03 18:12:22 -0500

    completed implementation of mergeSort() method

--- CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
+++ CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
@@ -289,12 +289,22 @@ namespace CSharpExamplesOnSorting
             tmp_pos = left;
             num_elements = (right - left + 1);
 
-            while((left <= left_end) && (mid <= right))
+            while ((left <= left_end) && (mid <= right))
             {
                 if (numbers[left] <= numbers[mid])
                     temp[tmp_pos++] = numbers[left++];
                 else
                     temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+            for(i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
             }
         }
 
