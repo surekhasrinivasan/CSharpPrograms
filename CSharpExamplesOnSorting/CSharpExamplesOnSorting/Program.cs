96e7f1d1144a6dc3a6ca611744350a64f1402ca2
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-04 17:58:53 -0500

    Program to Implement Quick Sort

--- CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
+++ CSharpExamplesOnSorting/CSharpExamplesOnSorting/Program.cs
@@ -273,57 +273,63 @@ namespace CSharpExamplesOnSorting
     //    }
     //}
 
-    // Program to Perform Merge Sort
+    //// Program to Perform Merge Sort
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+    //        int len = 9;
+    //        Console.WriteLine("MergeSort : ");
+    //        sortMethod(numbers, 0, len - 1);
+    //        for(int i = 0; i < 9; i++)
+    //            Console.WriteLine(numbers[i]);
+    //        Console.Read();
+    //    }
+
+    //    public static void mergeMethod(int[] numbers, int left, int mid, int right)
+    //    {
+    //        int[] temp = new int[25];
+    //        int i, left_end, num_elements, tmp_pos;
+    //        left_end = (mid - 1);
+    //        tmp_pos = left;
+    //        num_elements = (right - left + 1);
+
+    //        while ((left <= left_end) && (mid <= right))
+    //        {
+    //            if (numbers[left] <= numbers[mid])
+    //                temp[tmp_pos++] = numbers[left++];
+    //            else
+    //                temp[tmp_pos++] = numbers[mid++];
+    //        }
+
+    //        while (left <= left_end)
+    //            temp[tmp_pos++] = numbers[left++];
+    //        while (mid <= right)
+    //            temp[tmp_pos++] = numbers[mid++];
+    //        for(i = 0; i < num_elements; i++)
+    //        {
+    //            numbers[right] = temp[right];
+    //            right--;
+    //        }
+    //    }
+
+    //    public static void sortMethod(int[] numbers, int left, int right)
+    //    {
+    //        int mid;
+    //        if(right > left)
+    //        {
+    //            mid = (right + left) / 2;
+    //            sortMethod(numbers, left, mid);
+    //            sortMethod(numbers, (mid + 1), right);
+    //            mergeMethod(numbers, left, (mid + 1), right);
+    //        }
+    //    }
+    //}
+
+    // Program to Implement Quick Sort
     class Program
     {
-        static void Main()
-        {
-            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
-            int len = 9;
-            Console.WriteLine("MergeSort : ");
-            sortMethod(numbers, 0, len - 1);
-            for(int i = 0; i < 9; i++)
-                Console.WriteLine(numbers[i]);
-            Console.Read();
-        }
 
-        public static void mergeMethod(int[] numbers, int left, int mid, int right)
-        {
-            int[] temp = new int[25];
-            int i, left_end, num_elements, tmp_pos;
-            left_end = (mid - 1);
-            tmp_pos = left;
-            num_elements = (right - left + 1);
-
-            while ((left <= left_end) && (mid <= right))
-            {
-                if (numbers[left] <= numbers[mid])
-                    temp[tmp_pos++] = numbers[left++];
-                else
-                    temp[tmp_pos++] = numbers[mid++];
-            }
-
-            while (left <= left_end)
-                temp[tmp_pos++] = numbers[left++];
-            while (mid <= right)
-                temp[tmp_pos++] = numbers[mid++];
-            for(i = 0; i < num_elements; i++)
-            {
-                numbers[right] = temp[right];
-                right--;
-            }
-        }
-
-        public static void sortMethod(int[] numbers, int left, int right)
-        {
-            int mid;
-            if(right > left)
-            {
-                mid = (right + left) / 2;
-                sortMethod(numbers, left, mid);
-                sortMethod(numbers, (mid + 1), right);
-                mergeMethod(numbers, left, (mid + 1), right);
-            }
-        }
     }
 }
