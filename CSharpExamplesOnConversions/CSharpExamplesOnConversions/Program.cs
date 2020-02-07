1d43e1c51169f6a03524b2d327160de6fdb42524
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-07 17:39:51 -0500

    Program to Convert Decimal to Binary

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -6,26 +6,32 @@ using System.Threading.Tasks;
 
 namespace CSharpExamplesOnConversions
 {
-    // Program to Perform Binary to Decimal Conversion
+    //// Program to Perform Binary to Decimal Conversion
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int num, binary_value, decimal_value = 0, base_value = 1, rem;
+
+    //        Console.WriteLine("Enter a Binary Number(1s and 0s) : ");
+    //        num = int.Parse(Console.ReadLine()); // maximum five digits 
+    //        binary_value = num;
+    //        while(num > 0)
+    //        {
+    //            rem = num % 10;
+    //            decimal_value = decimal_value + rem * base_value;
+    //            num = num / 10;
+    //            base_value = base_value * 2;
+    //        }
+    //        Console.WriteLine("The Binary number is : " + binary_value );
+    //        Console.WriteLine("The Decimal number is : " + decimal_value);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to Convert Decimal to Binary
     class Program
     {
-        static void Main(string[] args)
-        {
-            int num, binary_value, decimal_value = 0, base_value = 1, rem;
 
-            Console.WriteLine("Enter a Binary Number(1s and 0s) : ");
-            num = int.Parse(Console.ReadLine()); // maximum five digits 
-            binary_value = num;
-            while(num > 0)
-            {
-                rem = num % 10;
-                decimal_value = decimal_value + rem * base_value;
-                num = num / 10;
-                base_value = base_value * 2;
-            }
-            Console.WriteLine("The Binary number is : " + binary_value );
-            Console.WriteLine("The Decimal number is : " + decimal_value);
-            Console.ReadLine();
-        }
     }
 }
