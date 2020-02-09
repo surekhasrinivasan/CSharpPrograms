0434efd679e87a60c53d5f08bf48aa9ceaee1a0c
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-08 23:02:19 -0500

    implementation of decimal to binary number

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -41,6 +41,17 @@ namespace CSharpExamplesOnConversions
             Console.WriteLine("The give decimal number is: " + num);
             int quot;
             string rem = "";
+            while(num >= 1)
+            {
+                quot = num / 2;
+                rem += (num % 2).ToString();
+                num = quot;
+            }
+            string bin = "";
+            for(int i = rem.Length - 1; i >= 0; i--)
+            {
+                bin = bin + rem[i];
+            }
         }
     }
 }
