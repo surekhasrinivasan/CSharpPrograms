d207d60e0814511f9c8b5b44ab332acc28ef6d9b
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-13 21:46:33 -0500

    Program to Obtain the Character from the User and Convert the Case of the Character-completed

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -148,6 +148,16 @@ namespace CSharpExamplesOnConversions
             int i;
             Console.WriteLine("Enter the Character : ");
             a = Convert.ToChar(Console.ReadLine());
+            i = (int)a;
+            if(a >= 65 && a <= 90)
+            {
+                Console.WriteLine("The Character is : {0}", char.ToLower(a));
+            }
+            else if(a >= 97 && a <= 122)
+            {
+                Console.WriteLine("The Character is : {0}", char.ToUpper(a));
+            }
+            Console.ReadLine();
         }
     }
 }
