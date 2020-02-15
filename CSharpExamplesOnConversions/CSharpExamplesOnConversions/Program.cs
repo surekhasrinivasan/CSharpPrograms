cbbc8bbaaef0a0559241441fed9d8c92cf3c87cd
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-15 13:10:17 -0500

    Program to Perform Currency Conversions

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -161,20 +161,26 @@ namespace CSharpExamplesOnConversions
     //    }
     //}
 
-    // Program to Convert a Given Number of Days in terms of Years, Weeks & Days
+    //// Program to Convert a Given Number of Days in terms of Years, Weeks & Days
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        int ndays, year, week, days, DAYSINWEEK = 7;
+    //        Console.WriteLine("Enter the number of days : ");
+    //        ndays = int.Parse(Console.ReadLine());
+    //        year = ndays / 365;
+    //        week = (ndays % 365) / DAYSINWEEK;
+    //        days = (ndays % 365) % DAYSINWEEK;
+    //        Console.WriteLine("{0} is equivalent to {1} years, {2} weeks and {3} days",
+    //                            ndays, year, week, days);
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    // Program to Perform Currency Conversions
     class Program
     {
-        static void Main()
-        {
-            int ndays, year, week, days, DAYSINWEEK = 7;
-            Console.WriteLine("Enter the number of days : ");
-            ndays = int.Parse(Console.ReadLine());
-            year = ndays / 365;
-            week = (ndays % 365) / DAYSINWEEK;
-            days = (ndays % 365) % DAYSINWEEK;
-            Console.WriteLine("{0} is equivalent to {1} years, {2} weeks and {3} days",
-                                ndays, year, week, days);
-            Console.ReadLine();
-        }
+
     }
 }
