24d96cecf947a523d550bee81f87c1f06e372f46
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-20 20:49:07 -0500

    Program to Perform Conversions of Meter to Kilometer and viceversa

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -236,33 +236,39 @@ namespace CSharpExamplesOnConversions
     //    }
     //}
 
-    // Program to Perform Decimal to HexaDecimal Conversion
+    //// Program to Perform Decimal to HexaDecimal Conversion
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        int decimalNumber, quotient;
+    //        int i = 1, j, temp=0;
+    //        char[] hexadecimalNumber = new char[100];
+    //        char temp1;
+    //        Console.WriteLine("Enter a Decimal Number : ");
+    //        decimalNumber = int.Parse(Console.ReadLine());
+    //        quotient = decimalNumber;
+    //        while(quotient != 0)
+    //        {
+    //            temp = quotient % 16;
+    //            if (temp < 10)
+    //                temp = temp + 48;
+    //            else
+    //                temp = temp + 55;
+    //            temp1 = Convert.ToChar(temp);
+    //            hexadecimalNumber[i++] = temp1;
+    //            quotient = quotient / 16;
+    //        }
+    //        Console.Write("Equivalent HexaDecimal Number is ");
+    //        for(j = i -1; j > 0; j--)
+    //            Console.Write(hexadecimalNumber[j]);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to Perform Conversions of Meter to Kilometer and viceversa
     class Program
     {
-        static void Main()
-        {
-            int decimalNumber, quotient;
-            int i = 1, j, temp=0;
-            char[] hexadecimalNumber = new char[100];
-            char temp1;
-            Console.WriteLine("Enter a Decimal Number : ");
-            decimalNumber = int.Parse(Console.ReadLine());
-            quotient = decimalNumber;
-            while(quotient != 0)
-            {
-                temp = quotient % 16;
-                if (temp < 10)
-                    temp = temp + 48;
-                else
-                    temp = temp + 55;
-                temp1 = Convert.ToChar(temp);
-                hexadecimalNumber[i++] = temp1;
-                quotient = quotient / 16;
-            }
-            Console.Write("Equivalent HexaDecimal Number is ");
-            for(j = i -1; j > 0; j--)
-                Console.Write(hexadecimalNumber[j]);
-            Console.Read();
-        }
+
     }
 }
