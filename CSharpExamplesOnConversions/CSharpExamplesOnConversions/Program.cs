907ee318bd5ca9ff94625cf3e9dba60d8da59cf7
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-24 21:05:01 -0500

    implementation of convert method

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -352,7 +352,55 @@ namespace CSharpExamplesOnConversions
             int prio = 0;
             postfix = "";
             Stack<Char> s1 = new Stack<char>();
-            
+            for (int i = 0; i < infix.Length; i++)
+            {
+                char ch = infix[i];
+                if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+                {
+                    if (s1.Count <= 0)
+                        s1.Push(ch);
+                    else
+                    {
+                        if (s1.Peek() == '*' || s1.Peek() == '/')
+                            prio = 1;
+                        else
+                            prio = 0;
+                        if (prio == 1)
+                        {
+                            if (ch == '+' || ch == '-')
+                            {
+                                postfix += s1.Pop();
+                                i--;
+                            }
+                            else
+                            {
+                                postfix += s1.Pop();
+                                i--;
+                            }
+                        }
+                        else
+                        {
+                            if (ch == '+' || ch == '-')
+                            {
+                                postfix += s1.Pop();
+                                s1.Push(ch);
+
+                            }
+                            else
+                                s1.Push(ch);
+                        }
+                    }
+                }
+                else
+                {
+                    postfix += ch;
+                }
+            }
+            int len = s1.Count;
+            for (int j = 0; j < len; j++)
+                postfix += s1.Pop();
+            return true;
         }
     }
-}
+} 
+
