bd9dfe8744465b2f643d782281e743c458145dc7
Surekhasrinivasan <surekhasrao@gmail.com>
2020-02-26 21:49:49 -0500

    Program to Demonstrate DefaultIfEmpty case

--- CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
+++ CSharpExamplesOnConversions/CSharpExamplesOnConversions/Program.cs
@@ -338,102 +338,108 @@ namespace CSharpExamplesOnConversions
     //    }
     //}
 
-    // Program to Convert Infix to Postfix.
-    // Here the infix expression is obtained from the user and is 
-    // converted to postfix expression which consists of primary 
-    // expressions or expressions in which postfix operators follow 
-    // a primary expression.
+    //// Program to Convert Infix to Postfix.
+    //// Here the infix expression is obtained from the user and is 
+    //// converted to postfix expression which consists of primary 
+    //// expressions or expressions in which postfix operators follow 
+    //// a primary expression.
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string infix = "";
+    //        string postfix = "";
+    //        if (args.Length == 1)
+    //        {
+    //            infix = args[0];
+    //            convert(ref infix, out postfix);
+    //            System.Console.WriteLine("InFix  :\t" + infix);
+    //            System.Console.WriteLine("PostFix:\t" + postfix);               
+    //        }
+    //        else
+    //        {
+    //            // testcase 1
+    //            infix = "a+b*c-d";
+    //            convert(ref infix, out postfix);
+    //            Console.WriteLine("InFix :\t" + infix);
+    //            Console.WriteLine("PostFix :\t" + postfix);
+    //            Console.WriteLine();
+    //            // testcase 2
+    //            infix = "a+b*c-d/e*f";
+    //            convert(ref infix, out postfix);
+    //            Console.WriteLine("InFix  :\t" + infix);
+    //            Console.WriteLine("PostFix:\t" + postfix);
+    //            Console.WriteLine();
+    //            // testcase 3
+    //            infix = "a-b/c*d-e--f/h*i++j-/k";
+    //            convert(ref infix, out postfix);
+    //            Console.WriteLine("InFix   :\t" + infix);
+    //            Console.WriteLine("PostFix :\t" + postfix);
+    //            Console.WriteLine();
+    //            Console.ReadLine();
+    //        }
+    //    }
+
+    //    static bool convert(ref string infix, out string postfix)
+    //    {
+    //        int prio = 0;
+    //        postfix = "";
+    //        Stack<Char> s1 = new Stack<char>();
+    //        for (int i = 0; i < infix.Length; i++)
+    //        {
+    //            char ch = infix[i];
+    //            if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+    //            {
+    //                if (s1.Count <= 0)
+    //                    s1.Push(ch);
+    //                else
+    //                {
+    //                    if (s1.Peek() == '*' || s1.Peek() == '/')
+    //                        prio = 1;
+    //                    else
+    //                        prio = 0;
+    //                    if (prio == 1)
+    //                    {
+    //                        if (ch == '+' || ch == '-')
+    //                        {
+    //                            postfix += s1.Pop();
+    //                            i--;
+    //                        }
+    //                        else
+    //                        {
+    //                            postfix += s1.Pop();
+    //                            i--;
+    //                        }
+    //                    }
+    //                    else
+    //                    {
+    //                        if (ch == '+' || ch == '-')
+    //                        {
+    //                            postfix += s1.Pop();
+    //                            s1.Push(ch);
+
+    //                        }
+    //                        else
+    //                            s1.Push(ch);
+    //                    }
+    //                }
+    //            }
+    //            else
+    //            {
+    //                postfix += ch;
+    //            }
+    //        }
+    //        int len = s1.Count;
+    //        for (int j = 0; j < len; j++)
+    //            postfix += s1.Pop();
+    //        return true;
+    //    }
+    //}
+
+    // Program to Demonstrate DefaultIfEmpty case
     class Program
     {
-        static void Main(string[] args)
-        {
-            string infix = "";
-            string postfix = "";
-            if (args.Length == 1)
-            {
-                infix = args[0];
-                convert(ref infix, out postfix);
-                System.Console.WriteLine("InFix  :\t" + infix);
-                System.Console.WriteLine("PostFix:\t" + postfix);               
-            }
-            else
-            {
-                // testcase 1
-                infix = "a+b*c-d";
-                convert(ref infix, out postfix);
-                Console.WriteLine("InFix :\t" + infix);
-                Console.WriteLine("PostFix :\t" + postfix);
-                Console.WriteLine();
-                // testcase 2
-                infix = "a+b*c-d/e*f";
-                convert(ref infix, out postfix);
-                Console.WriteLine("InFix  :\t" + infix);
-                Console.WriteLine("PostFix:\t" + postfix);
-                Console.WriteLine();
-                // testcase 3
-                infix = "a-b/c*d-e--f/h*i++j-/k";
-                convert(ref infix, out postfix);
-                Console.WriteLine("InFix   :\t" + infix);
-                Console.WriteLine("PostFix :\t" + postfix);
-                Console.WriteLine();
-                Console.ReadLine();
-            }
-        }
 
-        static bool convert(ref string infix, out string postfix)
-        {
-            int prio = 0;
-            postfix = "";
-            Stack<Char> s1 = new Stack<char>();
-            for (int i = 0; i < infix.Length; i++)
-            {
-                char ch = infix[i];
-                if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
-                {
-                    if (s1.Count <= 0)
-                        s1.Push(ch);
-                    else
-                    {
-                        if (s1.Peek() == '*' || s1.Peek() == '/')
-                            prio = 1;
-                        else
-                            prio = 0;
-                        if (prio == 1)
-                        {
-                            if (ch == '+' || ch == '-')
-                            {
-                                postfix += s1.Pop();
-                                i--;
-                            }
-                            else
-                            {
-                                postfix += s1.Pop();
-                                i--;
-                            }
-                        }
-                        else
-                        {
-                            if (ch == '+' || ch == '-')
-                            {
-                                postfix += s1.Pop();
-                                s1.Push(ch);
-
-                            }
-                            else
-                                s1.Push(ch);
-                        }
-                    }
-                }
-                else
-                {
-                    postfix += ch;
-                }
-            }
-            int len = s1.Count;
-            for (int j = 0; j < len; j++)
-                postfix += s1.Pop();
-            return true;
-        }
     }
-} 
+}
 
