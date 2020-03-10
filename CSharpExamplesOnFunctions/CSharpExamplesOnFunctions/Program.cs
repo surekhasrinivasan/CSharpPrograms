89c3555a918eab5be8fd007be88633cf0a16afb7
Surekhasrinivasan <surekhasrao@gmail.com>
2020-03-09 21:45:21 -0400

    Program to Demonstrate the Operations of C# Path Class

--- CSharpExamplesOnFunctions/CSharpExamplesOnFunctions/Program.cs
+++ CSharpExamplesOnFunctions/CSharpExamplesOnFunctions/Program.cs
@@ -91,33 +91,39 @@ namespace CSharpExamplesOnFunctions
     //    }
     //}
 
-    // Program to Find Sum of Digits of a Number using Recursion
+    //// Program to Find Sum of Digits of a Number using Recursion
+    //class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        int num, result;
+    //        pro pg = new pro();
+    //        Console.WriteLine("Enter the Number : ");
+    //        num = int.Parse(Console.ReadLine());
+    //        result = pg.sum(num);
+    //        Console.WriteLine("Sum of Digits in {0} is {1}", num, result);
+    //        Console.ReadLine();
+    //    }
+
+    //}
+    //class pro
+    //{
+    //    public int sum(int num)
+    //    {
+    //        if (num != 0)
+    //        {
+    //            return (num % 10 + sum(num / 10));
+    //        }
+    //        else
+    //        {
+    //            return 0;
+    //        }
+    //    }
+    //}
+
+    // Program to Demonstrate the Operations of C# Path Class
     class Program
     {
-        public static void Main()
-        {
-            int num, result;
-            pro pg = new pro();
-            Console.WriteLine("Enter the Number : ");
-            num = int.Parse(Console.ReadLine());
-            result = pg.sum(num);
-            Console.WriteLine("Sum of Digits in {0} is {1}", num, result);
-            Console.ReadLine();
-        }
 
-    }
-    class pro
-    {
-        public int sum(int num)
-        {
-            if (num != 0)
-            {
-                return (num % 10 + sum(num / 10));
-            }
-            else
-            {
-                return 0;
-            }
-        }
     }
 }
